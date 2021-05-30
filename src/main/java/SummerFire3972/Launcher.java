51362c9f88aeff3b81c086863134be8081f355df
1234tgk <1234tgk@gmail.com>
2021-05-30 17:20:11 -0400

    include launch4j

---
+++ src/main/java/SummerFire3972/Launcher.java
@@ -0,0 +1,10 @@
+package SummerFire3972;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		Application.launch(MainApp.class, args);
+	}
+}
